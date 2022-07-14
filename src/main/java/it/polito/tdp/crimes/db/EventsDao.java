9d7968be6446eae8b31484035e9a2d973d0cb9d9
davide <davide@mbp-di-davide>
2022-07-14 10:43:16 +0200

    a

--- src/main/java/it/polito/tdp/crimes/db/EventsDao.java
+++ src/main/java/it/polito/tdp/crimes/db/EventsDao.java
@@ -5,6 +5,7 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 import it.polito.tdp.crimes.model.Event;
 
@@ -46,6 +47,41 @@ public class EventsDao {
 			
 			conn.close();
 			return list ;
+
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return null ;
+		}
+	}
+
+	public List<String> getVertici(String categoria, int giorno) {
+		String sql = "SELECT DISTINCT offense_type_id "
+				+ "FROM events "
+				+ "WHERE offense_category_id = ? AND DAY(reported_date) = ? "
+				+ "ORDER BY offense_type_id ";
+		List<String> result = new ArrayList<>();
+		
+		try {
+			Connection conn = DBConnect.getConnection() ;
+
+			PreparedStatement st = conn.prepareStatement(sql) ;
+			st.setString(1, categoria);
+			st.setInt(2, giorno);
+			
+			ResultSet res = st.executeQuery() ;
+			
+			while(res.next()) {
+				try {
+					result.add(res.getString("offense_type_id"));
+				} catch (Throwable t) {
+					t.printStackTrace();
+					System.out.println(res.getInt("id"));
+				}
+			}
+			
+			conn.close();
+			return result ;
 
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
--- src/main/java/it/polito/tdp/crimes/model/Model.java
+++ src/main/java/it/polito/tdp/crimes/model/Model.java
@@ -1,6 +1,28 @@
 package it.polito.tdp.crimes.model;
 
+import org.jgrapht.Graph;
+import org.jgrapht.Graphs;
+import org.jgrapht.graph.DefaultWeightedEdge;
+import org.jgrapht.graph.SimpleWeightedGraph;
+
+import it.polito.tdp.crimes.db.EventsDao;
 
 public class Model {
+	private Graph<String, DefaultWeightedEdge> grafo;
+	private EventsDao dao;
 	
+	public Model() {
+		this.dao = new EventsDao();
+	}
+	
+	public void creaGrafo(String categoria, int giorno) {
+		this.grafo = new SimpleWeightedGraph<>(DefaultWeightedEdge.class);
+		
+		//aggiungo vertici
+		Graphs.addAllVertices(this.grafo, this.dao.getVertici(categoria, giorno));
+		
+		//Aggiungo archi
+		
+		
+	}
 }
